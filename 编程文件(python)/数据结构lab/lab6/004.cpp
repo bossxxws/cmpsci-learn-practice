cd8903ff88606bcfa7483f5eb6a796db2c00b7fe
bossxxws <2339294360@qq.com>
2024-05-30 11:49:13 +0800

    new

--- 编程文件(python)/数据结构lab/lab6/004.cpp
+++ 编程文件(python)/数据结构lab/lab6/004.cpp
@@ -1,9 +1,13 @@
-#include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
+unordered_map<int,int>s;
+
 int main()
 {
+    s[1]=2;
+    cout<<s[1]<<endl;
     cout<<11<<endl;
     return 0;
 }
---
+++ 编程文件(python)/数据结构lab/lab6/HelloWorld.java
@@ -0,0 +1,7 @@
+package lab6;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
