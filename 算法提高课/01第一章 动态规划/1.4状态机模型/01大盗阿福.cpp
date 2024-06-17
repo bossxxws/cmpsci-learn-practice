603a1ab3826ae8a89219e690b331790142d9a42d
bossxxws <2339294360@qq.com>
2024-06-17 22:59:00 +0800

    new

--- 算法提高课/01第一章 动态规划/1.4状态机模型/01大盗阿福.cpp
+++ 算法提高课/01第一章 动态规划/1.4状态机模型/01大盗阿福.cpp
@@ -2,8 +2,44 @@
 
 using namespace std;
 
+int t;
+
+int n;
+
+const int N=1e5+5;
+
+int a[N];
+
+int f[N][2];//f[i][j]中，i代表前i个金店的方案，j为1表示偷，j为0表示不偷
+//fi,1=fi−1,0+wi
+//fi,0=max(fi−1,1,fi−1,0)
 int main()
 {
+    cin>>t;
+
+    while(t-)
+    {
+        cin>>n;
+
+        for(int i=1;i<=n;i++)
+        {
+            cin>>a[i];//输入每家金店的现金数量
+        }
+
+        //dp
+        f[0][0]=0;
+        f[0][1]=-INF;
+
+        for(int i=1;i<=n;i++)
+        {
+            f[i][0] = max(f[i - 1][1], f[i - 1][0]);
+            f[i][1] = f[i - 1][0] + w[i];
+        }
+
+         cout << max(f[n][0], f[n][1]) << endl;
+    }
+
+
 
 
     return 0;
