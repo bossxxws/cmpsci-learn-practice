c7c1e0ea8a46349b60b1658bd10f1e3fe0f914f9
bossxxws <2339294360@qq.com>
2024-06-18 13:04:55 +0800

    new

--- 算法提高课/01第一章 动态规划/1.4状态机模型/01大盗阿福.cpp
+++ 算法提高课/01第一章 动态规划/1.4状态机模型/01大盗阿福.cpp
@@ -6,18 +6,21 @@ int t;
 
 int n;
 
-const int N=1e5+5;
+const int N=1e5+5,INF=0x3f3f3f3f;
 
 int a[N];
 
 int f[N][2];//f[i][j]中，i代表前i个金店的方案，j为1表示偷，j为0表示不偷
+
+//转移方程：
 //fi,1=fi−1,0+wi
 //fi,0=max(fi−1,1,fi−1,0)
+
 int main()
 {
     cin>>t;
 
-    while(t-)
+    while(t--)
     {
         cin>>n;
 
@@ -28,12 +31,12 @@ int main()
 
         //dp
         f[0][0]=0;
-        f[0][1]=-INF;
+        f[0][1]=0;
 
         for(int i=1;i<=n;i++)
         {
             f[i][0] = max(f[i - 1][1], f[i - 1][0]);
-            f[i][1] = f[i - 1][0] + w[i];
+            f[i][1] = f[i - 1][0] + a[i];
         }
 
          cout << max(f[n][0], f[n][1]) << endl;
---
+++ 算法提高课/01第一章 动态规划/1.4状态机模型/02股票买卖4.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int N=1e5+3;
+
+int n,k;
+
+int a[N];
+
+int main()
+{
+	cin>>n>>k;
+	
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i])	
+	}//读入股票在第i天的价格 
+	
+	//dp
+	
+	
+	return 0;
+}
+ w,,mm k. `
