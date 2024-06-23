e1f47404fae33eda7f1bc3b8ced35b67d1c125b9
奥 张 <2339294360@qq.com>
2024-06-23 21:35:51 +0800

    new

--- 算法提高课/01第一章 动态规划/1.5状态压缩dp/test.cpp
+++ 算法提高课/01第一章 动态规划/1.5状态压缩dp/test.cpp
@@ -2,14 +2,38 @@
 
 using namespace std;
 
-int main()
-{
-	
-	char z[4]="abc";
-	
-	char *x;
+void swap2(int* i, int* j);
+void rank(int* x, int* y);
+
+int main() {
+   int val1 = 15, val2 = 13;
+   printf("%d %d\n", val1, val2);
+   ///rank(&val1, &val2);
+   printf("%d %d\n", val1, val2);
+   char *x;
+	char y;
+	char z[4]=	"abc";
 	x=z;
-	cout<<x;
-	
-	return 0;
+	y=z[2];
+	z[0]=*(x+1);
+	cout<<"---"<<endl;
+	cout<<x[1]<<" "<<*(x+1)<<endl;
+	cout<<"---"<<endl;
+
+}
+
+void swap2(int* i, int* j) {
+  int temp;
+  temp = *i;
+  *i = *j;
+  *j = temp;
+}
+
+void rank(int* x, int* y) 
+{
+  int swaped = 0;
+  if(*x > *y){
+      swap2(x, y);
+      swaped = 1;
+  }
 }
