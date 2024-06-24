a7625a6f51e306a9d8115fb0410d7102962ef220
bossxxws <2339294360@qq.com>
2024-06-24 22:18:28 +0800

    new

--- Javaprojects/src/com/nefu/JavaTask.java
+++ Javaprojects/src/com/nefu/JavaTask.java
@@ -3,7 +3,32 @@ package com.nefu;
 import java.util.*;
 import java.io.*;
 
-class Student {
+// 功能需求:
+
+// 成绩录入:允许老师手动输入学生的成绩信息,包括学号、姓名、成绩。
+// 成绩查询:允许查看某个学生的成绩信息,或者查看所有学生的成绩信息。
+// 成绩修改:允许修改学生的成绩信息。
+// 成绩删除:允许删除学生的成绩信息。
+// 成绩统计:可以统计各个分数段的学生人数,并按照分数从高到低排序。
+// 等等....(程序可拓展性较好)
+// 数据结构设计:
+
+// 使用集合来存储学生成绩信息,选择HashSet
+// 每个学生的成绩信息存储为一个对象,包括学号、姓名和成绩。
+
+// 文件操作:
+// 将学生成绩信息保存到文件中,使用序列化的方式。
+// 在程序启动时,从文件中读取学生成绩数据到内存中的集合。
+// 在程序退出时,将内存中的学生成绩数据保存到文件中
+
+// 功能实现:
+// 录入成绩:输入学号、姓名和成绩,创建学生成绩对象并添加到集合中。
+// 查询成绩:根据学号查询某个学生的成绩信息,或者遍历集合显示所有学生的成绩信息。
+// 修改成绩:根据学号查找学生成绩对象,修改对象的成绩属性。
+// 删除成绩:根据学号从集合中删除学生成绩对象。
+// 成绩统计:遍历集合,统计各个分数段的学生人数,并按照分数从高到低排序输出。
+
+class Student implements Serializable {
     String id;
     String name;
     double score;
@@ -13,6 +38,13 @@ class Student {
         this.id = id;
         this.name = name;
         this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "学号：" + id + ", 姓名：" + name + ", 成绩：" + score
+                ;
     }
 }
 
@@ -39,6 +71,9 @@ class ScoreManager {
         Student student = queryScore(id);
         if (student != null) {
             student.score = newScore;
+            System.out.println("修改完毕");
+        } else {
+            System.out.println("学生不存在");
         }
     }
 
@@ -62,23 +97,35 @@ class ScoreManager {
 
     // 成绩排序
     public void sortScores() {
-        Collections.sort(students, new Comparator<Student>() {
-            public int compare(Student s1, Student s2) {
-                return Double.compare(s2.score, s1.score);
-            }
-        });
+        if (students.size() != 0) {
+            Collections.sort(students, new Comparator<Student>() {
+                public int compare(Student s1, Student s2) {
+                    return Double.compare(s2.score, s1.score);
+                }
+            });
+        }
+
     }
 
     // 分数段统计
     public void scoreSegmentStatistics() {
         int[] segments = new int[10];
-        for (Student student : students) {
-            int segment = (int)student.score / 10;
-            segments[segment]++;
+        if (students.size() != 0) {
+            for (Student student : students) {
+                if (student.score == 100) {
+                    segments[9]++;
+                } else {
+                    int segment = (int) student.score / 10;
+                    segments[segment]++;
+                }
+            }
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i * 10 + "-" + (i + 1) * 10 + ": " + segments[i]);
+            }
+        } else {
+            System.out.println("数据为空");
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i*10 + "-" + (i+1)*10 + ": " + segments[i]);
-        }
+
     }
 
     // 保存到文件
@@ -94,8 +141,143 @@ class ScoreManager {
     public void loadFromFile(String filename) throws IOException, ClassNotFoundException {
         FileInputStream fis = new FileInputStream(filename);
         ObjectInputStream ois = new ObjectInputStream(fis);
-        students = (List<Student>)ois.readObject();
+        students = (List<Student>) ois.readObject();
         ois.close();
         fis.close();
+    }
+
+    //输出排序后的成绩
+    void PrintSortedGrades() {
+        sortScores();
+        if (students.size() != 0) {
+            for (Student t : students) {
+                System.out.println(t);
+            }
+        } else {
+            System.out.println("数据为空");
+        }
+
+    }
+
+    void deleteAll() {
+        for (int i = 0; i < students.size(); i++) {
+            students.remove(i);
+        }
+    }
+}
+
+public class JavaTask {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        ScoreManager manager = new ScoreManager();
+        manager.loadFromFile("scores.txt");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("******这是第六小组的PBL作业******");
+
+        System.out.println("本实验由第六小组成员共同完成");
+        System.out.println("小组成员：张奥、颜煦桐、刘雅馨、郭贝儿、刘思昀（排名不分先后）");
+        String c;
+        System.out.println("是否进入学生成绩管理系统？（y/n）");
+        c = scanner.next();
+
+        if (c.equals("y")) {
+            System.out.println("正在进入学生成绩管理系统......");
+            Thread.sleep(2000);
+            System.out.println("欢迎来到学生成绩管理系统！");
+            while (true) {
+                int operation;
+                System.out.println("--------------------------------------------------------------------------");
+                System.out.println("请输入操作：");
+                System.out.println("000： 保存并退出");
+                System.out.println("001： 录入成绩分");
+                System.out.println("002： 查询成绩");
+                System.out.println("003： 修改成绩");
+                System.out.println("004： 删除成绩");
+                System.out.println("005： 统计平均成绩");
+                System.out.println("006： 从高到低显示所有学生成绩");
+                System.out.println("007： 分数段统计");
+                System.out.println("008： 查询成绩最高的学生");
+                System.out.println("009： 删除所有成绩");
+                System.out.println("010： 保存");
+
+                System.out.println("按任意非数字键退出");
+                System.out.println("--------------------------------------------------------------------------");
+                if (scanner.hasNextInt()) {
+                    operation = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (operation == 1) {
+                        System.out.println("请输入学生ID：");
+                        String id = scanner.nextLine();
+                        System.out.println("请输入学生姓名：");
+                        String name = scanner.nextLine();
+                        System.out.println("请输入学生成绩：");
+                        double score = scanner.nextDouble();
+                        scanner.nextLine();
+                        manager.addScore(id, name, score);
+                        System.out.println("录入成功");
+                        Thread.sleep(2000);
+                    } else if (operation == 2) {
+                        System.out.println("请输入学生ID：");
+                        String id = scanner.nextLine();
+                        Student student = manager.queryScore(id);
+                        if (student != null) {
+                            System.out.println("学生ID: " + student.id + ", 姓名: " + student.name + ", 成绩: " + student.score);
+                        } else {
+                            System.out.println("未找到该学生的成绩。");
+                        }
+                        Thread.sleep(2000);
+                    } else if (operation == 3) {
+                        System.out.println("请输入学生ID：");
+                        String id = scanner.nextLine();
+                        System.out.println("请输入新的成绩：");
+                        double newScore = scanner.nextDouble();
+                        scanner.nextLine();
+                        manager.modifyScore(id, newScore);
+                        Thread.sleep(2000);
+                    } else if (operation == 4) {
+                        System.out.println("请输入学生ID：");
+                        String id = scanner.nextLine();
+                        manager.deleteScore(id);
+                        System.out.println("删除完毕");
+                        Thread.sleep(2000);
+                    } else if (operation == 5) {
+                        manager.statistics();
+                        Thread.sleep(2000);
+                    } else if (operation == 6) {
+                        manager.PrintSortedGrades();
+                        Thread.sleep(2000);
+                    } else if (operation == 7) {
+                        manager.scoreSegmentStatistics();
+                        Thread.sleep(2000);
+                    } else if (operation == 8) {
+                        manager.sortScores();
+                        if (manager.students.size() != 0) System.out.println(manager.students.get(0));
+                        else System.out.println("数据为空");
+                        Thread.sleep(2000);
+                    } else if (operation == 9) {
+                        manager.deleteAll();
+                        System.out.println("已删除所有成绩数据");
+                        Thread.sleep(2000);
+                    } else if (operation == 10) {
+                        manager.saveToFile("scores.txt");
+                        System.out.println("成功保存");
+                        Thread.sleep(2000);
+                    } else if (operation == 000) {
+                        manager.saveToFile("scores.txt");
+                        System.out.println("成功保存，正在退出......");
+                        break;
+                    } else {
+                        System.out.println("无效的操作，请重新输入。");
+                        Thread.sleep(2000);
+                    }
+                } else {
+                    System.out.println("正在退出......");
+                    break;
+                }
+            }
+
+            scanner.close();
+        }
     }
 }
--- 算法提高课/01第一章 动态规划/1.5状态压缩dp/03炮兵阵地.cpp
+++ 算法提高课/01第一章 动态规划/1.5状态压缩dp/03炮兵阵地.cpp
@@ -8,7 +8,10 @@ int g[130];
 
 int n,m; 
 
-int f[2][M][M];
+int f[2][M][M];//滚动数组，行数那一列开为2就行了
+//滚动数组原理：
+//因为行数是奇偶交换的，并且状态转移只依赖于上一行的状态
+//所以说&1就可以区分本行和上一行，然后取最大值，所以说用滚动数组是可行的 
 
 vector<int>legal;
 //vector<int>trans[M];
@@ -24,9 +27,11 @@ bool check(int state)
 	return true;
 }
 
+//计算出这一行有多少个炮兵
 int count(int state)
 {
 	int res=0;
+//	cout<<1<<endl;
 	while(state)
 	{
 		if(state&1)res++;
@@ -37,8 +42,9 @@ int count(int state)
 
 int main()
 {
-	int n,m;
-	
+
+	cin>>n>>m;
+	 
 	for(int i=1;i<=n;i++)
 	{
 		for(int j=0;j<m;j++)//因为二进制表示要从第0位开始，所以j只能从0开始 
@@ -58,35 +64,46 @@ int main()
 			cnt[i]=count(i);
 		}
 	} 
-	
+//	cout<<"--"<<endl; 
 	//预处理合法转移状态同时进行dp 
 	for(int i=1;i<=n+2;i++)
 	{
 		//枚举三行的状态 
-		for(int j=0;j<legal.size();i++)
+		for(int j=0;j<legal.size();j++)
 		{
 			for(int k=0;k<legal.size();k++)
 			{
 				for(int u=0;u<legal.size();u++)
 				{
-					int a=legal[j];//当前行 
-					int b=legal[k];//上一行 
-					int c=legal[u];//上上行 
+					int a=legal[j];//当前行 i 
+					int b=legal[k];//上一行 i-1 
+					int c=legal[u];//上上行 i-2
 					if(a & b || a&c || b&c)continue;//三行都不能出现有炮兵在一列
 					//出现就不是合法形式，直接continue下一个 
 					
 					//炮兵不能放在山坡上
-					if(g[i-1] &a || g[i]&b) continue;
+					//i-2的情况也要算上
+					//加一个特判就行了 
+					if(i>=2)if(g[i]&a|| g[i-1] &b || g[i-2]&c) continue;
+					else
+					{
+						if(g[i]&a || g[i-1]&b)continue;
+					}
 					
 					//如果合法就进行状态转移
 					
-					f[i&1][j][k]=max(f[i&1][j][k],f[i-1 &1][u][j]+cnt[b]);
+					//dp 
+					//不取当前状态 和 取当前状态 中取最大值 
+					//取当前状态的情况下：
+					//上一行的最大值加上当前行的数量
+					//然后和其他状态下最大的f（不取当前行的状态）取max 
+					f[i&1][j][k]=max(f[i&1][j][k],f[i-1 &1][k][u]+cnt[a]);
 					
 				}
 			}	
 		}	
 	} 
-	
+//	cout<<"--"<<endl; 
 	cout<<f[n+2&1][0][0]<<endl;
 	
 	return 0;
