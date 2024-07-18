8624dd4bbefa592f5d13ece186899791254346b5
bossxxws <2339294360@qq.com>
2024-07-19 00:28:59 +0800

    new

---
+++ 算法提高课/01第一章 动态规划/1.6区间dp/test.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	cout<<"œ„≤›À≠£ø"<<endl<<"1.¿Ó≥ø—Û"<<endl<<"2.≤‹Ó£√˘"<<endl;
+	string s;
+	cin>>s;
+	if(s=="¿Ó≥ø—Û")
+	{
+	
+	} 
+	return 0;
+}
